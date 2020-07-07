0de8b6bb2214df1627ad295ed6520e61955e96c3
Ihor-malyshko <malishkoio@gmail.com>
2020-07-07 15:32:24 +0300

    Updates

--- src.fb1f0bef.js
+++ src.fb1f0bef.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-html-2/src.c701a409.js.map
+//# sourceMappingURL=/project-html-2/src.fb1f0bef.js.map
